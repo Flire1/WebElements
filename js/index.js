b2b0fa0c4de9d6e7e676a48dfb8ee9ca39dbdc8e
Flire1 <e.flire1@gmail.com>
2025-03-11 22:55:59 -0400

    Fix Attempt 2: Pages

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ const doc = document;
 
 doc.addEventListener('DOMContentLoaded', () => {
     doc.getElementById('title').addEventListener('click', () => {
-        window.location.href = '/index.html';
+        window.location.href = 'WebElements';
     });
 
     doc.getElementById('buttons').addEventListener('click', () => {
-        window.location.href = 'buttons.html';
+        window.location.href = 'WebElements/buttons.html';
     });
 });
